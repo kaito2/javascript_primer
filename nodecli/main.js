f729a7bc6c9bc60b13ee19db53987e6f6d008fe3
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-07-01 21:15:28 +0900

    p380

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,1 +1,19 @@
-console.log("hello world!");
+// commander モジュールを program としてインポートする
+program = require("commander");
+// fs モジュールを fs オブジェクトとしてインポートする
+fs = require("fs");
+
+// コマンドライン引数を commander でパースする
+program.parse(process.argv);
+// ファイルパスを program.args から取り出す
+const filePath = program.args[0];
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+    // 終了ステータス1 (一般的なエラー) としてプロセス終了する
+    process.exit(1);
+  }
+  console.log(file);
+});
