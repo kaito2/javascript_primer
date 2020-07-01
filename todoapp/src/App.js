38d0cb48287149702973e247f370a2d38f60e31e
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-07-02 01:32:03 +0900

    p408

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -3,4 +3,15 @@ export class App {
   constructor() {
     console.log("App initialized");
   }
+
+  mount() {
+    const formElement = document.querySelector("#js-form");
+    const inputElement = document.querySelector("#js-form-input");
+    formElement.addEventListener("submit", (event) => {
+      // submit イベントの本来の動作を止める
+      // -> preventDefault をしない場合はページがリロードされる
+      event.preventDefault(); // そんなオプションが…
+      console.log(`入力欄の値: ${inputElement.value}`);
+    });
+  }
 }
