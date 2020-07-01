028ad2f348b06f6315268db8cab81e22ed88a961
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-07-02 01:54:45 +0900

    p413

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,3 +1,5 @@
+import { element } from "./view/html-util.js";
+
 console.log("App.js: loaded");
 export class App {
   constructor() {
@@ -7,11 +9,28 @@ export class App {
   mount() {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
+    const containerElement = document.querySelector("#js-todo-list");
+    const todoItemCounterElement = document.querySelector("#js-todo-count");
+    // Todo アイテム数
+    let todoItemCount = 0;
+
     formElement.addEventListener("submit", (event) => {
       // submit イベントの本来の動作を止める
       // -> preventDefault をしない場合はページがリロードされる
       event.preventDefault(); // そんなオプションが…
-      console.log(`入力欄の値: ${inputElement.value}`);
+
+      // 追加する Todo アイテムの要素(li 要素)を作成する
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+
+      // Todo アイテムを container に追加する
+      containerElement.appendChild(todoItemElement);
+
+      // Todo アイテム数を +1 し、表示されているテキストを更新する
+      todoItemCount += 1;
+      todoItemCounterElement.textContent = `Todo アイテム数: ${todoItemCount}`;
+
+      // 入力欄を空文字列にしてリセット
+      inputElement.value = "";
     });
   }
 }
---
+++ todoapp/src/view/html-util.js
@@ -0,0 +1,45 @@
+export function escapeSpecialChars(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+/**
+ * HTML 文字列から HTML 要素を作成して返す
+ * @param {string} html
+ */
+export function htmlToElement(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template.content.firstElementChild;
+}
+
+/**
+ * HTML 文字列から DOM Node を作成して返すタグ関数
+ */
+export function element(strings, ...values) {
+  const htmlString = strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialChars(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+  return htmlToElement(htmlString);
+}
+
+/**
+ * コンテナ要素の中身を bodyElement で上書きする
+ * @param {Element} bodyElement コンテナの中身となる要素
+ * @param {Element} containerElement コンテナ要素
+ */
+export function render(bodyElement, containerElement) {
+  // containerElement の中身を空にする
+  containerElement.innerHTML = "";
+  // containerElement の直下に bodyElement を追加する
+  containerElement.appendChild(bodyElement);
+}
